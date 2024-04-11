5d380bb437acb69eca63b20859ac5da68b4f32d0
Bharatparm <160983111+Bharatparm@users.noreply.github.com>
2024-04-11 14:04:11 -0400

    Update lab3.sh

--- lab3.sh
+++ lab3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script runs the configure-host.sh script from the current directory to modify 2 servers and update the local /etc/hosts file
+
 # Function to execute a command and check if it succeeded
 execute() {
     if ! "$@"; then
@@ -8,38 +10,26 @@ execute() {
     fi
 }
 
-# Function to transfer and run configure-host.sh script on a remote server
-configure_remote_server() {
-    local server_address="$1"
-    local hostname="$2"
-    local ip_address="$3"
-    local entry_name="$4"
-    local entry_ip="$5"
+# Transfer configure-host.sh script to the remote servers
+execute scp configure-host.sh remoteadmin@server1-mgmt:/root
+execute scp configure-host.sh remoteadmin@server2-mgmt:/root
 
-    # Transfer configure-host.sh script to the remote server
-    execute scp configure-host.sh remoteadmin@"$server_address":/root
+# Run configure-host.sh script on the remote servers
+if [ "$1" = "-verbose" ]; then
+    execute ssh -v remoteadmin@server1-mgmt -- sudo /root/configure-host.sh -name loghost -ip 192.168.16.3 -he webhost 192.168.16.4
+    execute ssh -v remoteadmin@server2-mgmt -- sudo /root/configure-host.sh -name webhost -ip 192.168.16.4 -he loghost 192.168.16.3
+else
+    execute ssh remoteadmin@server1-mgmt -- sudo /root/configure-host.sh -name loghost -ip 192.168.16.3 -he webhost 192.168.16.4
+    execute ssh remoteadmin@server2-mgmt -- sudo /root/configure-host.sh -name webhost -ip 192.168.16.4 -he loghost 192.168.16.3
+fi
 
-    # Run configure-host.sh script on the remote server
-    execute ssh remoteadmin@"$server_address" -- sudo /root/configure-host.sh -name "$hostname" -ip "$ip_address" -hostentry "$entry_name" "$entry_ip"
-}
-
-# Update local /etc/hosts file
-update_local_hosts() {
-    local entry_name="$1"
-    local entry_ip="$2"
-
-    # Run configure-host.sh locally
-    execute sudo ./configure-host.sh -hostentry "$entry_name" "$entry_ip"
-}
-
-# Configure server1-mgmt
-configure_remote_server "server1-mgmt" "loghost" "192.168.16.3" "webhost" "192.168.16.4"
-
-# Configure server2-mgmt
-configure_remote_server "server2-mgmt" "webhost" "192.168.16.4" "loghost" "192.168.16.3"
-
-# Update local /etc/hosts file
-update_local_hosts "loghost" "192.168.16.3"
-update_local_hosts "webhost" "192.168.16.4"
+# Run configure-host.sh script locally
+if [ "$1" = "-verbose" ]; then
+    ./configure-host.sh -v -hostentry loghost 192.168.16.3
+    ./configure-host.sh -v -hostentry webhost 192.168.16.4
+else
+    ./configure-host.sh -hostentry loghost 192.168.16.3
+    ./configure-host.sh -hostentry webhost 192.168.16.4
+fi
 
 echo "All operations completed successfully."
